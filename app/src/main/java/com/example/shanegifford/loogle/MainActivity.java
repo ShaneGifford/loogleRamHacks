08990768e5c8a4d27e6eb9c3c9a88bf3cfd9fbc5
Shane Gifford <giffordsm@vcu.edu>
2017-09-16 17:07:05 -0400

    Initial project files

--- app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
+++ app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
@@ -10,4 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickFilters() {
+        //send to Filters activity
+    }
+
+    public void onClickEmergency() {
+        //send to Emergency activity
+    }
 }
