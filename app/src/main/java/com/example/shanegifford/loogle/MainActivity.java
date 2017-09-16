f534b43c61f7f7040a831628548ae2eeab6b55a9
Shane Gifford <giffordsm@vcu.edu>
2017-09-16 18:03:40 -0400

    Location services (hopefully) enabled

--- app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
+++ app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shanegifford.loogle;
 
 import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +33,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!service.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_LONG).show();
+        }
     }
 }
