17ea82ba4b35dd1a0e2671bde6b7b148cfbdeee2
Shane Gifford <giffordsm@vcu.edu>
2017-09-16 17:32:24 -0400

    Filter screen added

--- app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
+++ app/src/main/java/com/example/shanegifford/loogle/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shanegifford.loogle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,13 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickFilters() {
-        //send to Filters activity
-    }
+        Button btn_filters = (Button) findViewById(R.id.button_filters);
+        btn_filters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FilterActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void onClickEmergency() {
-        //send to Emergency activity
+        Button btn_emergency = (Button) findViewById(R.id.button_emergency);
+        btn_filters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FilterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
